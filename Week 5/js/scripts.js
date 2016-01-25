2f712673c43960fe02174106c4c53cee2366663c
TAYLOR HIDEN <TAYLOR@ALLEGIANCESTRATEGIES.COM>
2016-01-25 09:02:33 -0500

    Add js to citipix page

---
+++ Week 5/js/scripts.js
@@ -0,0 +1,47 @@
+/* User types in name of city
+Get value of city name
+User presses submit
+Show background of new city */
+
+$(document).ready(function() {
+
+$('form').submit(changeCity);
+
+//when the user pushes the submit button
+//the background of the document will change
+
+function changeCity() {
+	event.preventDefault();
+
+	var city = $('#city-type').val();
+
+	//if the city is NYC, change to nyc.jpg
+	//if the city is SF, chance to sf.jpg
+	//if the city is LA, change to la.jpg
+	//if the city is Austin, change to austin.jpg
+	//if the city is Sydney, change to sydney.jpg
+
+	if (city == 'NYC'|| 'New York City'||'New York') { 
+		$('body').attr('background');
+		$('body').addClass('nyc');
+	} else if (city == 'San Francisco'|| 'SF'|| 'Bay Area') {
+		$('body').attr('background');
+		$('body').addClass('sf');
+	} else if (city == 'Los Angeles'|| 'LA'|| 'LAX') {
+		$('body').attr('background');
+		$('body').addClass('la');
+	} else if (city == 'Austin'|| 'ATX') {
+		$('body').attr('background');
+		$('body').addClass('austin');
+	} else if (city == 'Sydney'|| 'SYD') {
+		$('body').attr('background');
+		$('body').addClass('sydney');
+	}
+
+
+
+}
+
+
+
+});

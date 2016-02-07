bc115e63364f4ff3135d1980f56e45e3f2cdca67
Joni Halabi <joni@jhalabi.com>
2016-02-07 11:43:44 -0500

    Feedback for homework #6

--- Week 6/js/scripts.js
+++ Week 6/js/scripts.js
@@ -1,3 +1,15 @@
+/*
+ * Hi Taylor!  I am including feedback about your code here in inline Javascript comments.
+ * 
+ * Overall, the logic of your JS code here looks great!  
+ * There was one syntax error with your code - specifically with the punctuation at the 
+ * bottom of the file.  You have an extra end curly brace, which I commented out below.  
+ * Once I commented this out, your code works perfectly!
+ *
+ * Make sure the indentation of your code is consistent - that can really help you find issues
+ * like extra punctuation easier to find.
+ */
+
 $(document).ready(function() {
 
    // Form validation goes here!
@@ -7,6 +19,7 @@ $(document).ready(function() {
    function checkEverything(event) {
    		event.preventDefault();
 
+      /* Great use of multiple functions here - it is really good form to have one function for one task. */
    		checkName();
    		checkEmail();
       checkPassword();
@@ -19,6 +32,14 @@ $(document).ready(function() {
 
        var lastName = $('#last').val();
 
+       /* 
+        * This if statement is just great. 
+        * One comment about the conditionals, you can also say:
+        *    if (!firstName)
+        * The "!" means "not", which means here "if there is nothing in the firstName variable"
+        * Exactly the same as what you are saying here (firstName == false).
+        */
+       
        if (firstName == false) {
            alert("Type in your first name!!");
        } else if (lastName == false) {
@@ -36,7 +57,9 @@ $(document).ready(function() {
    		if (email == false) {
    		 alert('Invalid email');
    		} else {
-   			
+   			/* If you do not want anything to happen if the conditional in the "if" statement is false, you
+           do not need to have the "else" statement here.
+         */
    		}
    }
 
@@ -53,6 +76,6 @@ $(document).ready(function() {
 
 
 
-   }
+   // }  <-- extra end curly brace causing a syntax error.
 
 });
